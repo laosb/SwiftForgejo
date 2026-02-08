da8a21a1217bcef31ab56f1afef347439b5c0988
laosb <git@lao.sb>
2026-02-08 21:16:05 +0800

    chore: Remove async HTTP client dependency and expand platform support
    - Downgrade Swift tools version from 6.1 to 6.0
    - Add support for iOS 13, watchOS 6, and tvOS 13
    - Remove `swift-openapi-async-http-client` dependency
    - Update `Package.resolved` to reflect dependency changes

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.1
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,10 @@ import PackageDescription
 let package = Package(
   name: "SwiftForgejo",
   platforms: [
-    .macOS(.v10_15)
+    .macOS(.v10_15),
+    .iOS(.v13),
+    .watchOS(.v6),
+    .tvOS(.v13),
   ],
   products: [
     // Products define the executables and libraries a package produces, making them visible to other packages.
@@ -17,14 +20,12 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.10.0"),
     .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.8.0"),
-    .package(url: "https://github.com/swift-server/swift-openapi-async-http-client", from: "1.1.0"),
   ],
   targets: [
     .target(
       name: "Forgejo",
       dependencies: [
-        .byName(name: "ForgejoAPI"),
-        .product(name: "OpenAPIAsyncHTTPClient", package: "swift-openapi-async-http-client"),
+        .byName(name: "ForgejoAPI")
       ],
     ),
     .target(
